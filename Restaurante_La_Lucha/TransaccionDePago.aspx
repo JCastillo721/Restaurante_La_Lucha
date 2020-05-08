64800206f327683769b85fd91c247e3a844c182d
JCastillo721 <vladimircastillo14@yahoo.es>
2020-05-08 11:57:38 -0600

    Update

--- Restaurante_La_Lucha/TransaccionDePago.aspx
+++ Restaurante_La_Lucha/TransaccionDePago.aspx
@@ -2,5 +2,14 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-
+    <br /><br />
+    <h2>Digite los datos de su tarjeta de Cr&eacute;dito o D&eacute;bito</h2>
+    <asp:TextBox ID="TextBox1" runat="server" placeholder="Número de tarjeta" BorderColor="#CCCCCC"></asp:TextBox><br />
+    <asp:TextBox ID="TextBox2" runat="server" placeholder="MM/AA" BorderColor="#CCCCCC"></asp:TextBox>
+    <asp:TextBox ID="TextBox3" runat="server" placeholder="CVV" BorderColor="#CCCCCC"></asp:TextBox>
+    <asp:TextBox ID="TextBox4" runat="server" placeholder="Nombre según tarjeta" BorderColor="#CCCCCC"></asp:TextBox>
+    <asp:TextBox ID="TextBox5" runat="server" placeholder="Email" BorderColor="#CCCCCC"></asp:TextBox>
+    <asp:Label ID="Label1" runat="server" Text="Monto: "></asp:Label><asp:Label ID="Label2" runat="server" Text="0.00"></asp:Label>
+    <br />
+    <asp:Button ID="Button1" runat="server" Text="Pagar" />
 </asp:Content>
--- Restaurante_La_Lucha/TransaccionDePago.aspx.designer.cs
+++ Restaurante_La_Lucha/TransaccionDePago.aspx.designer.cs
@@ -13,5 +13,77 @@ namespace Restaurante
 
     public partial class TransaccionDePago
     {
+
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// Control TextBox2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// Control TextBox3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+
+        /// <summary>
+        /// Control TextBox4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+
+        /// <summary>
+        /// Control TextBox5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox5;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
